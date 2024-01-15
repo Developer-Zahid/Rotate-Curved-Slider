e5f80f09b526eb6a37e1d547a3a8230a956f11fe
Developer-Zahid <coderexpertzahid@gmail.com>
2024-01-15 15:40:04 +0600

    Added custom navigations

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,8 +33,6 @@ $(document).ready(function () {
             })
         })
 
-        console.log($allSlidesArray.findLast(item=> $(item).hasClass('slick-active')));
-
         let beforeFirstActiveSlideIndex = $allSlidesArray.indexOf($allSlidesArray.find(item=> $(item).hasClass('slick-active'))) - 1
         let afterLastActiveSlideIndex = $allSlidesArray.indexOf($allSlidesArray.findLast(item=> $(item).hasClass('slick-active'))) + 1
 
@@ -52,9 +50,16 @@ $(document).ready(function () {
 
     $slider.slick({
         infinite: true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/></svg></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/></svg></button>',
+        appendArrows: $('.slider__navigation'),
+        appendDots: $('.slider__navigation'),
         slidesToShow: 5,
         slidesToScroll: 1,
         centerMode: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
         centerPadding: '0px',
         speed: 100,
         responsive: [
